3770e486a9c8090bc97f732fc4614613b3b15008
yousef1110ya <yousefalshikhali1711@gmail.com>
2025-06-17 16:23:48 +0300

    added the ability to handle multipul users
    where we create a new thread for each connection to the system.

--- Main.java
+++ Main.java
@@ -1,47 +1,58 @@
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Main{
-	public static void main(String[] args){
-		ServerSocket serverSocket = null ; 
-		Socket clientSocket = null ;
-		int port = 6379;
-		try{
-			serverSocket = new ServerSocket(port);
-			serverSocket.setReuseAddress(true);
+public class Main {
+  public static void main(String[] args) {
+    int port = 6379;
+    boolean listening = true;
 
-			clientSocket = serverSocket.accept();
-			while(true){
-				// Read the input from client
-				clientSocket.getOutputStream().write("java-redis: > ".getBytes());
-				byte[] input = new byte[1024];
-				clientSocket.getInputStream().read(input);
-				String inputString = new String(input).trim();
-				System.out.println("recived:" + inputString);
-				String res = inputString.replace("PING" , "").trim();
-				if (res.isEmpty()) {
-					res = "PONG";
-				}
-				res = res + "\r\n";
-				clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
+      serverSocket.setReuseAddress(true);
 
-			}
-		}catch(IOException e ){
-			System.out.println("catched an IOException error ");
-			System.out.println(e.getMessage());
-		
-		}finally{
-			try{
-				if(clientSocket != null){
-					clientSocket.close();
-				}
-			} catch(IOException e){
-				System.out.println("catched an IOException while closing the clientSocket");
-				System.out.println(e.getMessage());
-			}
+      while (listening) {
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("New client connected");
+
+        new Thread(() -> handleClient(clientSocket))
+            .start(); // todo replace by threadpool later
+      }
+
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+      System.exit(-1);
+    }
+  }
+
+  static void handleClient(Socket clientSocket) {
+    try (clientSocket; // automatically closes socket at the end
+         OutputStream outputStream = clientSocket.getOutputStream();
+         BufferedReader in = new BufferedReader(
+             new InputStreamReader(clientSocket.getInputStream()))) {
+
+      while (true) {
+        // hacky way to read input; will change
+        outputStream.write("redis-cli/>".getBytes());
+        // if (in.readLine() == null) {
+        //   break;
+        // }
+        String line = in.readLine();
+        System.out.println("Last line: " + line);
+
+        outputStream.write("+PONG\r\n".getBytes());
+        System.out.println("Wrote pong");
+      }
+
+    } catch (IOException e) {
+		if ("Broken pipe".equals(e.getMessage())) {
+    		System.out.println("user disconnected");
+		}else {
+      		System.out.println("IOException: " + e.getMessage());
+
 		}
-	}
-
-
+    }
+  }
 }
